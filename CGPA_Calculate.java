7544f2c96fee8a435f0b6a71091f45d55fd41174
KrishSardhara013 <krishsardhara1312006@gmail.com>
2024-12-25 11:33:30 +0530

    Add files via upload

---
+++ CGPA_Calculate.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+
+        System.out.println("Enter Marks of subjects:");
+        int a=56;
+        int b=89;
+        int c=96;
+        int sum = ((a+b+c)/3);
+        System.out.println(sum);
+    }
+}
