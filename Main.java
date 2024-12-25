32f00047e971817bd12df3417afcacc106bb6797
KrishSardhara013 <krishsardhara1312006@gmail.com>
2024-12-25 11:12:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+    System.out.print("Sum of these number is :");
+    int a=5;
+    int b=6;
+    int c=7;
+    int sum= a+b+c;
+    System.out.print(sum);
+    }
+}
